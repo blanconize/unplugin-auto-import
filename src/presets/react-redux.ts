b181ad6596c418e37abdb1f8ba57fe5bdb1f1cb6
Matthias Weiß <45752327+blanconize@users.noreply.github.com>
2022-09-29 11:33:41 +0200

    Create react-redux.ts
    Adding React Redux to presets.

---
+++ src/presets/react-redux.ts
@@ -0,0 +1,17 @@
+import type { ImportsMap } from '../types'
+
+export const ReactReduxHooks = [
+  'batch',
+  'connect',
+	'Provider',
+  'useSelector',
+	'useDispatch',
+	'useStore',
+	'shallowEqual',
+]
+
+export default <ImportsMap>({
+  'react-redux': [
+    ...ReactReduxHooks,
+  ],
+})
